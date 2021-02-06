e32ced0ace96369832102a659a3583b92ce73173
Adam Scarberry <adam.scarberry@usace.army.mil>
2021-02-05 19:49:35 -0500

    renamed function for param config, add instr func

--- plugins/helpers/midas.py
+++ plugins/helpers/midas.py
@@ -8,11 +8,21 @@ def get_connection():
 
     return conn
 ################################################################    
-def get_aware_instruments(): 
+def get_aware_param_config(): 
 
     conn = get_connection()
     h = HttpHook(http_conn_id=conn.conn_id, method='GET')    
     endpoint = '/aware/data_acquisition_config'
+    headers = {"Content-Type": "application/json"}
+    r = h.run(endpoint=endpoint, headers=headers)
+
+    return json.dumps(r.json())
+################################################################ 
+def get_aware_instruments(): 
+
+    conn = get_connection()
+    h = HttpHook(http_conn_id=conn.conn_id, method='GET')    
+    endpoint = '/projects/82c07c9a-9ec8-4ff5-850c-b1d74ffb5e14/instruments'
     headers = {"Content-Type": "application/json"}
     r = h.run(endpoint=endpoint, headers=headers)
 
