a9d09e43999705e16620332eb64ab7216d283182
Adam Scarberry <adam.scarberry@usace.army.mil>
2021-03-18 18:58:26 -0400

    test api call to change task status

---
+++ api-test.py
@@ -0,0 +1,22 @@
+import requests
+
+API_ENDPOINT = 'http://localhost:8000/api/v1'
+
+dag_id = 'cumulus_download_and_process_snodas'
+dag_run_id = 'scheduled__2014-01-01T01:00:00+00:00'
+task_id = 'snodas_process_cogs'
+
+task_update_endpoint = f'{API_ENDPOINT}/dags/{dag_id}/updateTaskInstancesState'
+headers={"Content-Type": "application/json"}
+payload = {
+  "dry_run": False,
+  "execution_date": "2014-01-01T01:00:00+00:00",
+  "include_downstream": False,
+  "include_future": False,
+  "include_past": False,
+  "include_upstream": False,
+  "new_state": "failed",
+  "task_id": task_id
+}
+r = requests.post(task_update_endpoint, json=payload, auth=('airflow', 'airflow'))
+print(r.text)
