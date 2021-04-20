3631bf438b6b3302f87035df4f348bc1d020d1cc
Adam Scarberry <adam.scarberry@usace.army.mil>
2021-04-20 15:35:11 -0400

    fix product name

--- dags/cumulus__acq_ncep_rtma2p5_ru_anl.py
+++ dags/cumulus__acq_ncep_rtma2p5_ru_anl.py
@@ -43,7 +43,7 @@ def cumulus_rtma_ru_anl_airtemp():
     """
 
     URL_ROOT = f'https://nomads.ncep.noaa.gov/pub/data/nccf/com/rtma/prod'
-    PRODUCT_SLUG = 'ncep-rtma-ru-anl'
+    PRODUCT_SLUG = 'ncep-rtma-ru-anl-airtemp'
     # S3_KEY_DIR = f'cumulus/ncep_rtma_ru_anl'
     SCHED_INTERVAL = 15
     LOOKBACK_INTERVALS = 0
