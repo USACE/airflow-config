9980f72b3964592ae4b8550b17126ae196618fda
Adam Scarberry <adam.scarberry@usace.army.mil>
2021-06-01 09:40:28 -0400

    add temp to develop/stable

--- dags/cumulus__copy_s3_wrf.py
+++ dags/cumulus__copy_s3_wrf.py
@@ -32,7 +32,7 @@ default_args = {
     'end_date': datetime(2018, 1, 1)
 }
 
-@dag(default_args=default_args, schedule_interval='@yearly', tags=['cumulus', 'precip'])
+@dag(default_args=default_args, schedule_interval='@yearly', tags=['cumulus', 'precip', 'airtemp'])
 def cumulus_copy_columbia_wrf():
     """This pipeline handles download, processing, and derivative product creation for \n
     Columbia WRF\n
@@ -42,33 +42,62 @@ def cumulus_copy_columbia_wrf():
     S3_SRC_BUCKET = 'columbia-river'
     S3_SRC_KEY_PREFIX = 'wrfout/d03fmt/reconstruction'
     S3_DST_BUCKET = 'cwbi-data-stable'
-    PRODUCT_SLUG = 'wrf-columbia-precip'
 
     @task()
     def copy_precip_s():
         execution_date = get_current_context()['execution_date']
+        product_slug = 'wrf-columbia-precip'
 
         src_filename = 'PRECIPAH.nc'
         dst_filename = f'precipah_{execution_date.strftime("%Y")}_s.nc'
         src_s3_key = f'{S3_SRC_KEY_PREFIX}/{execution_date.strftime("%Y")}s/ncf/{src_filename}'
-        dst_s3_key = f'{cumulus.S3_ACQUIRABLE_PREFIX}/{PRODUCT_SLUG}/{dst_filename}'
+        dst_s3_key = f'{cumulus.S3_ACQUIRABLE_PREFIX}/{product_slug}/{dst_filename}'
         print(f'Copying {src_s3_key} to {dst_s3_key}')
         copy_s3_file(S3_SRC_BUCKET, src_s3_key, S3_DST_BUCKET, dst_s3_key)                
 
-        return json.dumps({"datetime":execution_date.isoformat(), "s3_key":dst_s3_key})
+        return json.dumps({"datetime":execution_date.isoformat(), "s3_key":dst_s3_key, "product_slug":product_slug})
 
     @task()
     def copy_precip_w():
         execution_date = get_current_context()['execution_date']
+        product_slug = 'wrf-columbia-precip'
 
         src_filename = 'PRECIPAH.nc'
         dst_filename = f'precipah_{execution_date.strftime("%Y")}_w.nc'
         src_s3_key = f'{S3_SRC_KEY_PREFIX}/{execution_date.strftime("%Y")}w/ncf/{src_filename}'
-        dst_s3_key = f'{cumulus.S3_ACQUIRABLE_PREFIX}/{PRODUCT_SLUG}/{dst_filename}'
+        dst_s3_key = f'{cumulus.S3_ACQUIRABLE_PREFIX}/{product_slug}/{dst_filename}'
         print(f'Copying {src_s3_key} to {dst_s3_key}')
         copy_s3_file(S3_SRC_BUCKET, src_s3_key, S3_DST_BUCKET, dst_s3_key) 
 
-        return json.dumps({"datetime":execution_date.isoformat(), "s3_key":dst_s3_key})
+        return json.dumps({"datetime":execution_date.isoformat(), "s3_key":dst_s3_key, "product_slug":product_slug})
+
+    @task()
+    def copy_airtemp_s():
+        execution_date = get_current_context()['execution_date']
+        product_slug = 'wrf-columbia-airtemp'
+
+        src_filename = 'T2______.nc'
+        dst_filename = f't2_airtemp_{execution_date.strftime("%Y")}_s.nc'
+        src_s3_key = f'{S3_SRC_KEY_PREFIX}/{execution_date.strftime("%Y")}s/ncf/{src_filename}'
+        dst_s3_key = f'{cumulus.S3_ACQUIRABLE_PREFIX}/{product_slug}/{dst_filename}'
+        print(f'Copying {src_s3_key} to {dst_s3_key}')
+        copy_s3_file(S3_SRC_BUCKET, src_s3_key, S3_DST_BUCKET, dst_s3_key)                
+
+        return json.dumps({"datetime":execution_date.isoformat(), "s3_key":dst_s3_key, "product_slug":product_slug})
+
+    @task()
+    def copy_airtemp_w():
+        execution_date = get_current_context()['execution_date']
+        product_slug = 'wrf-columbia-airtemp'
+
+        src_filename = 'T2______.nc'
+        dst_filename = f't2_airtemp_{execution_date.strftime("%Y")}_w.nc'
+        src_s3_key = f'{S3_SRC_KEY_PREFIX}/{execution_date.strftime("%Y")}w/ncf/{src_filename}'
+        dst_s3_key = f'{cumulus.S3_ACQUIRABLE_PREFIX}/{product_slug}/{dst_filename}'
+        print(f'Copying {src_s3_key} to {dst_s3_key}')
+        copy_s3_file(S3_SRC_BUCKET, src_s3_key, S3_DST_BUCKET, dst_s3_key) 
+
+        return json.dumps({"datetime":execution_date.isoformat(), "s3_key":dst_s3_key, "product_slug":product_slug})
 
     @task()
     def notify_cumulus(payload):
@@ -77,15 +106,16 @@ def cumulus_copy_columbia_wrf():
         payload = json.loads(payload)
     
         cumulus.notify_acquirablefile(
-            acquirable_id=cumulus.acquirables[PRODUCT_SLUG], 
+            acquirable_id=cumulus.acquirables[payload['product_slug']],
             datetime=payload['datetime'], 
             s3_key=payload['s3_key'],
             conn_type='stable'
             )
 
-
     notify_cumulus(copy_precip_s())
     notify_cumulus(copy_precip_w())
 
+    notify_cumulus(copy_airtemp_s())
+    notify_cumulus(copy_airtemp_w())
 
 wrf_dag = cumulus_copy_columbia_wrf()
