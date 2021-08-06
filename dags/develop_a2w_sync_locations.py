a5d1ef7fa35bca7aa78a7ece30ddff70a6ded5ed
Jeff Gregory <jeffrey.s.gregory@me.com>
2021-08-06 08:36:13 -0500

    Raise exception for HTML returns

--- dags/develop_a2w_sync_locations.py
+++ dags/develop_a2w_sync_locations.py
@@ -15,8 +15,7 @@ from textwrap import dedent
 
 from airflow.models.dag import DAG
 from airflow.models.variable import Variable
-from airflow.utils.db import provide_session
-from airflow.models import XCom
+from airflow import AirflowException
 
 
 # Default arguments
@@ -29,63 +28,63 @@ default_args = {
 }
 # Get RADAR Locations
 def radar_locations(ti, office, format: str = 'json'):
-    url = Variable.get('CWMSDATA') + f'/locations?office={office.symbol}&name=@&format={format}'
+    cwms_data = Variable.get('CWMSDATA')
+    url = f'{cwms_data}/locations?office={office.symbol}&name=@&format={format}'
+    location_kinds = json.loads(water.get_location_kind())
     with requests.Session() as s:
         r = s.get(url)
-        t = r.text
-    return t
+        radar_locations_str = r.text
 
-def parse_locations(ti, office: str):
-    location_kinds = json.loads(water.get_location_kind())
+    if 'DOCTYPE html' in radar_locations_str:
+        raise AirflowException
+    else:
+        _radar_locations_str = radar_locations_str.translate(radar_locations_str.maketrans('', '', '\n\r\t')).replace('None', '')
+        loc_json = json.loads(_radar_locations_str)
+        locations = loc_json['locations']['locations']
+        
+        post_locations = list()
+        for location in locations:
+            # Get the kind_id from the location-kind list
+            if location['classification']['location-kind'] == 'SITE':
+                kind_id = [
+                    kind['id']
+                    for kind in location_kinds
+                    if kind['name'] == location['classification']['location-kind']
+                ]
+                # Create a Location dataclass
+                loc = radar.Location(
+                    office_id = office.id,
+                    name = location['identity']['name'],
+                    public_name = location['label']['public-name'],
+                    kind_id = kind_id[0]
+                )
+                # Create a Geometry dataclass with middle of North America as defaults
+                geo = radar.Geometry()
+                lat = location['geolocation']['latitude']
+                lon  = location['geolocation']['longitude']
+                if isinstance(lat, float): geo.latitude = lat
+                if isinstance(lon, float): geo.longitude = lon
+                # Create a Political dataclass to get the bounding office
+                political = radar.Political()
+                political.bounding_office = location['political']['bounding-office']
 
-    radar_locations_str = ti.xcom_pull()
-    _radar_locations_str = radar_locations_str.translate(radar_locations_str.maketrans('', '', '\n\r\t')).replace('None', '')
-    loc_json = json.loads(_radar_locations_str)
-    locations = loc_json['locations']['locations']
-    
-    post_locations = list()
-    for location in locations:
-        # Get the kind_id from the location-kind list
-        if location['classification']['location-kind'] == 'SITE':
-            kind_id = [
-                kind['id']
-                for kind in location_kinds
-                if kind['name'] == location['classification']['location-kind']
-            ]
-            # Create a Location dataclass
-            loc = radar.Location(
-                office_id = office.id,
-                name = location['identity']['name'],
-                public_name = location['label']['public-name'],
-                kind_id = kind_id[0]
-            )
-            # Create a Geometry dataclass with middle of North America as defaults
-            geo = radar.Geometry()
-            lat = location['geolocation']['latitude']
-            lon  = location['geolocation']['longitude']
-            if isinstance(lat, float): geo.latitude = lat
-            if isinstance(lon, float): geo.longitude = lon
-            # Create a Political dataclass to get the bounding office
-            political = radar.Political()
-            political.bounding_office = location['political']['bounding-office']
-
-            # Appending dictionary to post
-            post_locations.append(
-                {
-                    'office_id': loc.office_id,
-                    'name': loc.name,
-                    'public_name': loc.public_name,
-                    'kind_id': loc.kind_id,
-                    'geometry' : {
-                        'type': geo.type,
-                        'coordinates': [
-                            geo.latitude,
-                            geo.longitude
-                        ]
+                # Appending dictionary to post
+                post_locations.append(
+                    {
+                        'office_id': loc.office_id,
+                        'name': loc.name,
+                        'public_name': loc.public_name,
+                        'kind_id': loc.kind_id,
+                        'geometry' : {
+                            'type': geo.type,
+                            'coordinates': [
+                                geo.latitude,
+                                geo.longitude
+                            ]
+                        }
                     }
-                }
-            )
-    return json.dumps(post_locations)
+                )
+        return json.dumps(post_locations)
 
 def post_locations(ti, conn_type: str='develop'):
     payload = json.loads(ti.xcom_pull())
@@ -99,33 +98,19 @@ def post_locations(ti, conn_type: str='develop'):
             print(err, '\n', p)
             continue
 
-@provide_session
-def cleanup_xcom(context, session=None):
-    dag_id = context["ti"]["dag_id"]
-    session.query(XCom).filter(XCom.dag_id == dag_id).delete()
-
 # Assign result to variable 'dag'
 with DAG(
-    default_args=default_args, dag_id='RADAR_SYNC',
-    tags=['RADAR-SYNC'],
-    on_success_callback=cleanup_xcom,
+    default_args=default_args, dag_id='RADAR_SYNC_LOCATIONS',
+    tags=['RADAR-SYNC-LOCATIONS'],
 ) as dag:
     offices = json.loads(sharedApi.get_offices())
     for _office in offices:
         office = radar.Office(**_office)
-        if office.symbol is not None and office.symbol == 'LRH':
+        if office.symbol is not None:
             # Get the RADAR locations task to XCOM
             task_get_radar_locations = PythonOperator(
                 task_id=f'radar_locations_{office.symbol}',
                 python_callable=radar_locations,
-                op_kwargs={
-                    'office': office,
-                }
-            )
-            # Parse the XCOM return_value returning a list of payloads
-            task_parse_radar_locations = PythonOperator(
-                task_id=f'parse_locations_{office.symbol}',
-                python_callable=parse_locations,
                 op_kwargs={
                     'office': office,
                 }
@@ -136,4 +121,4 @@ with DAG(
                 python_callable=post_locations,
             )
 
-            task_get_radar_locations >> task_parse_radar_locations >> task_post_radar_locations
+            task_get_radar_locations >> task_post_radar_locations
