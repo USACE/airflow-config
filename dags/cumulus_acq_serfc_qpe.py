d0aef087110a589db1c6ffff22cd8fec2309665b
Jeff Gregory <jeffrey.s.gregory@me.com>
2021-08-12 10:52:29 -0500

    QPE start 5 days back worked better

--- dags/cumulus_acq_serfc_qpe.py
+++ dags/cumulus_acq_serfc_qpe.py
@@ -36,7 +36,7 @@ implementation = {
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': (datetime.utcnow()-timedelta(days=6)).replace(minute=0, second=0),
+    'start_date': (datetime.utcnow()-timedelta(days=5)).replace(minute=0, second=0),
     'catchup_by_default': False,
     'email_on_failure': False,
     'email_on_retry': False,
