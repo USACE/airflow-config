249aea1d51de230bbd83b0c3f547cd3f0406399e
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2021-11-09 16:50:01 -0500

    begin migration to castle cloud, remove develop, use env vars (#70)
    * begin migration to castle cloud, remove develop, use env vars
    
    * begin migration to castle cloud, remove develop, use env vars

--- dags/cumulus/hrrr_precip.py
+++ dags/cumulus/hrrr_precip.py
@@ -1,4 +1,3 @@
-
 import json
 import requests
 import logging
@@ -9,35 +8,27 @@ from airflow import DAG
 
 from airflow import AirflowException
 from datetime import datetime, timedelta
-from airflow.hooks.base_hook import BaseHook
-from airflow.providers.http.hooks.http import HttpHook
-from airflow.operators.python import task, get_current_context
+from airflow.operators.python import get_current_context
 
 # Operators; we need this to operate!
 from airflow.operators.python import PythonOperator
-from airflow.utils.dates import days_ago
 
 from helpers.downloads import trigger_download
 import helpers.cumulus as cumulus
 
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
-default_args = {
-    'owner': 'airflow',
-    'retries': 6,
-    'retry_delay': timedelta(minutes=10)
-}
+default_args = {"owner": "airflow", "retries": 6, "retry_delay": timedelta(minutes=10)}
 with DAG(
-    'develop_cumulus_hrrr_precip',
+    "cumulus_hrrr_precip",
     default_args=default_args,
-    description='HRRR Forecast Precip',
+    description="HRRR Forecast Precip",
     # start_date=(datetime.utcnow()-timedelta(hours=72)).replace(minute=0, second=0),
-    start_date=(datetime.utcnow()-timedelta(hours=2)).replace(minute=0, second=0),
-    tags=['cumulus', 'precip', 'develop', 'forecast'],    
+    start_date=(datetime.utcnow() - timedelta(hours=2)).replace(minute=0, second=0),
+    tags=["cumulus", "precip", "forecast"],
     # schedule_interval='*/15 * * * *'
-    schedule_interval='@hourly',
-    catchup=False
-    
+    schedule_interval="@hourly",
+    catchup=False,
 ) as dag:
     dag.doc_md = """This pipeline handles download and API notification for HRRR hourly forecast products. \n
     High-Resolution Rapid Refresh (HRRR) \n
@@ -48,22 +39,28 @@ with DAG(
     Files matching hrrr.t{HH}z.wrfsfcf{HH}.grib2 - Multiple hourly files (second variable) per forecast file (first variable)
     """
 
-    URL_ROOT = f'https://noaa-hrrr-bdp-pds.s3.amazonaws.com'
-    S3_BUCKET = 'cwbi-data-develop'
-    PRODUCT_SLUG = 'hrrr-total-precip'
+    URL_ROOT = f"https://noaa-hrrr-bdp-pds.s3.amazonaws.com"
+    PRODUCT_SLUG = "hrrr-total-precip"
     ##############################################################################
     def download_precip_fcst_hour(hour):
 
-        exec_dt = get_current_context()['execution_date']
+        exec_dt = get_current_context()["execution_date"]
 
         directory = f'hrrr.{exec_dt.strftime("%Y%m%d")}/conus'
-        src_filename = f'hrrr.t{exec_dt.strftime("%H")}z.wrfsfcf{str(hour).zfill(2)}.grib2'
+        src_filename = (
+            f'hrrr.t{exec_dt.strftime("%H")}z.wrfsfcf{str(hour).zfill(2)}.grib2'
+        )
         dst_filename = f'hrrr.{exec_dt.strftime("%Y%m%d")}.t{exec_dt.strftime("%H")}z.wrfsfcf{str(hour).zfill(2)}.grib2'
-        s3_key = f'{cumulus.S3_ACQUIRABLE_PREFIX}/{PRODUCT_SLUG}/{dst_filename}'
-        print(f'Downloading {src_filename}')
-        output = trigger_download(url=f'{URL_ROOT}/{directory}/{src_filename}', s3_bucket=S3_BUCKET, s3_key=s3_key)
+        s3_key = f"{cumulus.S3_ACQUIRABLE_PREFIX}/{PRODUCT_SLUG}/{dst_filename}"
+        print(f"Downloading {src_filename}")
+        output = trigger_download(
+            url=f"{URL_ROOT}/{directory}/{src_filename}",
+            s3_bucket=cumulus.S3_BUCKET,
+            s3_key=s3_key,
+        )
 
-        return json.dumps({"datetime":exec_dt.isoformat(), "s3_key":s3_key})
+        return json.dumps({"datetime": exec_dt.isoformat(), "s3_key": s3_key})
+
     ##############################################################################
     def notify_api(payload):
 
@@ -72,34 +69,36 @@ with DAG(
         # print(f'payload is: {payload}')
 
         cumulus.notify_acquirablefile(
-            acquirable_id=cumulus.acquirables[PRODUCT_SLUG], 
-            datetime=payload['datetime'], 
-            s3_key=payload['s3_key'],
-            conn_type='develop'
-            )        
+            acquirable_id=cumulus.acquirables[PRODUCT_SLUG],
+            datetime=payload["datetime"],
+            s3_key=payload["s3_key"],
+        )
 
         return
+
     ##############################################################################
     for fcst_hour in range(0, 19):
-        
+
         print(f"Forecast Hour: {fcst_hour}")
-        
+
         download_task_id = f"download_fcst_hr_{str(fcst_hour).zfill(2)}"
-        
+
         download_task = PythonOperator(
-            task_id=download_task_id, 
-            python_callable=download_precip_fcst_hour, 
+            task_id=download_task_id,
+            python_callable=download_precip_fcst_hour,
             op_kwargs={
-                'hour': str(fcst_hour).zfill(2),
-            }
+                "hour": str(fcst_hour).zfill(2),
+            },
         )
 
         notify_api_task = PythonOperator(
-            task_id=f"notify_api_fcst_hr_{str(fcst_hour).zfill(2)}",           
+            task_id=f"notify_api_fcst_hr_{str(fcst_hour).zfill(2)}",
             python_callable=notify_api,
-            op_kwargs={               
-                'payload': "{{{{task_instance.xcom_pull(task_ids='{}')}}}}".format(download_task_id)
-                }
+            op_kwargs={
+                "payload": "{{{{task_instance.xcom_pull(task_ids='{}')}}}}".format(
+                    download_task_id
+                )
+            },
         )
 
         download_task >> notify_api_task
