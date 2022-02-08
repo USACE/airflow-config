91d673ee5aa21f9d8789471576357325c1128361
Jeff Gregory <jeffrey.s.gregory@me.com>
2022-02-08 08:39:59 -0600

    NBM max hour and MRMS rename
    - NBM DAG has `MAX_HOUR` variable to change number of forecasts
    
    - NBM `region` variable added for possibly allow Alaska, Carib, Hawaii,
    etc.
    
    - TaskGroup added to group each forecast hour tasks
    
    - MRMS method name changed to `cumulus_mrmsv12()`
    
    - MRMS also now uses TaskGroup

--- dags/cumulus/mrms_v12.py
+++ dags/cumulus/mrms_v12.py
@@ -11,6 +11,7 @@ import json
 from string import Template
 from airflow.decorators import dag, task
 from airflow.operators.python import get_current_context
+from airflow.utils.task_group import TaskGroup
 from helpers.downloads import trigger_download
 
 import helpers.cumulus as cumulus
@@ -29,10 +30,10 @@ default_args = {
 
 @dag(
     default_args=default_args,
-    tags=["stable", "cumulus", "precip", "MRMS"],
+    tags=["cumulus", "precip", "MRMS"],
     schedule_interval="5 * * * *",
 )
-def mrms_v12():
+def cumulus_mrms_v12():
     """
     # Multi-Radar/Multi-Sensor V12
 
@@ -59,86 +60,93 @@ def mrms_v12():
 
     url_root = "https://mrms.ncep.noaa.gov/data/2D"
     products = {
-        "conus_p1": {
-            "slug": "ncep-mrms-v12-multisensor-qpe-01h-pass1",
-            "suffix": "MultiSensor_QPE_01H_Pass1",
-            "pass": 1,
-        },
-        "conus_p2": {
-            "slug": "ncep-mrms-v12-multisensor-qpe-01h-pass2",
-            "suffix": "MultiSensor_QPE_01H_Pass2",
-            "pass": 2,
-        },
-        "alaska_p1": {
-            "slug": "ncep-mrms-v12-msqpe01h-p1-alaska",
-            "suffix": "ALASKA/MultiSensor_QPE_01H_Pass1",
-            "pass": 1,
-        },
-        "alaska_p2": {
-            "slug": "ncep-mrms-v12-msqpe01h-p2-alaska",
-            "suffix": "ALASKA/MultiSensor_QPE_01H_Pass2",
-            "pass": 2,
-        },
-        "carib_p1": {
-            "slug": "ncep-mrms-v12-msqpe01h-p1-carib",
-            "suffix": "CARIB/MultiSensor_QPE_01H_Pass1",
-            "pass": 1,
-        },
-        "carib_p2": {
-            "slug": "ncep-mrms-v12-msqpe01h-p2-carib",
-            "suffix": "CARIB/MultiSensor_QPE_01H_Pass2",
-            "pass": 2,
-        },
+        "conus": [
+            {
+                "slug": "ncep-mrms-v12-multisensor-qpe-01h-pass1",
+                "suffix": "MultiSensor_QPE_01H_Pass1",
+                "pass": 1,
+            },
+            {
+                "slug": "ncep-mrms-v12-multisensor-qpe-01h-pass2",
+                "suffix": "MultiSensor_QPE_01H_Pass2",
+                "pass": 2,
+            },
+        ],
+        "alaska": [
+            {
+                "slug": "ncep-mrms-v12-msqpe01h-p1-alaska",
+                "suffix": "ALASKA/MultiSensor_QPE_01H_Pass1",
+                "pass": 1,
+            },
+            {
+                "slug": "ncep-mrms-v12-msqpe01h-p2-alaska",
+                "suffix": "ALASKA/MultiSensor_QPE_01H_Pass2",
+                "pass": 2,
+            },
+        ],
+        "carib": [
+            {
+                "slug": "ncep-mrms-v12-msqpe01h-p1-carib",
+                "suffix": "CARIB/MultiSensor_QPE_01H_Pass1",
+                "pass": 1,
+            },
+            {
+                "slug": "ncep-mrms-v12-msqpe01h-p2-carib",
+                "suffix": "CARIB/MultiSensor_QPE_01H_Pass2",
+                "pass": 2,
+            },
+        ],
     }
 
     # create dynamic tasks
-    def create_task(**kwargs):
-        t_id = kwargs["task_id"]
+    def create_task_group(name, products):
 
-        @task(task_id=t_id)
-        def download():
-            product_slug = kwargs["task_values"]["slug"]
-            url_suffix = kwargs["task_values"]["suffix"]
-            file_dir = f"{url_root}/{url_suffix}"
-            execution_date = get_current_context()["execution_date"]
-            filename = filename_template.substitute(
-                pass_=kwargs["task_values"]["pass"],
-                datetime_=execution_date.strftime("%Y%m%d-%H0000"),
-            )
-            s3_key = f"{key_prefix}/{product_slug}/{filename}"
+        with TaskGroup(group_id=f"{name}_mrmsv12") as task_group:
+            for product in products:
+                p_slug = product["slug"]
+                p_suffix = product["suffix"]
+                p_pass = product["pass"]
 
-            print(f"Downloading {filename}")
+                @task(task_id=f"download_pass{p_pass}")
+                def download():
+                    file_dir = f"{url_root}/{p_suffix}"
+                    execution_date = get_current_context()["execution_date"]
+                    filename = filename_template.substitute(
+                        pass_=p_pass,
+                        datetime_=execution_date.strftime("%Y%m%d-%H0000"),
+                    )
+                    s3_key = f"{key_prefix}/{p_slug}/{filename}"
 
-            trigger_download(
-                url=f"{file_dir}/{filename}", s3_bucket=s3_bucket, s3_key=s3_key
-            )
+                    print(f"Downloading {filename}")
 
-            return json.dumps(
-                {
-                    "datetime": execution_date.isoformat(),
-                    "s3_key": s3_key,
-                    "product_slug": product_slug,
-                }
-            )
+                    trigger_download(
+                        url=f"{file_dir}/{filename}", s3_bucket=s3_bucket, s3_key=s3_key
+                    )
 
-        @task(task_id=f"{t_id}_notify_cumulus")
-        def notify(payload):
-            payload_json = json.loads(payload)
-            result = cumulus.notify_acquirablefile(
-                acquirable_id=cumulus.acquirables[payload_json["product_slug"]],
-                datetime=payload_json["datetime"],
-                s3_key=payload_json["s3_key"],
-            )
-            print(result)
+                    return json.dumps(
+                        {
+                            "datetime": execution_date.isoformat(),
+                            "s3_key": s3_key,
+                            "product_slug": p_slug,
+                        }
+                    )
 
-        download_ = download()
-        notify_ = notify(download_)
+                @task(task_id=f"notify_pass{p_pass}")
+                def notify(payload):
+                    payload_json = json.loads(payload)
+                    result = cumulus.notify_acquirablefile(
+                        acquirable_id=cumulus.acquirables[payload_json["product_slug"]],
+                        datetime=payload_json["datetime"],
+                        s3_key=payload_json["s3_key"],
+                    )
+                    print(result)
 
-        return notify_
+                download_ = download()
+                notify(download_)
 
-    _ = [
-        create_task(task_id=t_id, task_values=t_val) for t_id, t_val in products.items()
-    ]
+            return task_group
+
+    _ = [create_task_group(key, val) for key, val in products.items()]
 
 
-DAG_ = mrms_v12()
+DAG_ = cumulus_mrms_v12()
