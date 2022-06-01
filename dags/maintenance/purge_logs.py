5f43d2e653c832d2edda24fdbe004881b9abdd7e
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-06-01 18:16:39 -0400

    new dag to purge local logs

---
+++ dags/maintenance/purge_logs.py
@@ -0,0 +1,50 @@
+"""
+### Purge Logs
+
+Airflow doesn't have a built in log rotation/purge (as of v2.1).  This DAG will delete and logs on disk older than 2 days.
+
+We already write to remote logs (via S3)
+"""
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+from airflow.operators.python import PythonOperator
+import logging
+import subprocess
+from textwrap import dedent
+
+with DAG(
+    dag_id="maint_purge_logs",
+    schedule_interval="@daily",
+    start_date=datetime(2022, 6, 2),
+    catchup=False,
+    tags=["maintenance"],
+    doc_md=dedent(__doc__),
+) as dag:
+
+    def display_storage():
+
+        bashCommand = "df -h && echo '----- /opt/airflow BREAKDOWN--------' && du -d 1 -h $AIRFLOW_HOME"
+        ret = subprocess.run(bashCommand, capture_output=True, shell=True)
+        lines = ret.stdout.decode().splitlines()
+        for line in lines:
+            logging.error(line)
+
+    def purge_logs():
+
+        display_storage()
+        bashCommand = "echo '\n-- Deleting files older than 2 days --' && find $AIRFLOW_HOME/logs -mtime +2 -exec rm -fv {} \;"
+        ret = subprocess.run(bashCommand, capture_output=True, shell=True)
+        lines = ret.stdout.decode().splitlines()
+        for line in lines:
+            logging.error(line)
+        display_storage()
+
+    purge_task = PythonOperator(
+        task_id="display_task_id",
+        python_callable=purge_logs,
+    )
+
+    purge_task
