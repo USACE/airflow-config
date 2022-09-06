a054527984a7172752f184d8774b4cb2c05c75cb
Jeff Gregory <jeffrey.s.gregory@me.com>
2022-09-06 15:17:22 -0500

    Rename acquirable and retiries is 1

--- dags/cumulus/wrf_british_columbia_copy_s3.py
+++ dags/cumulus/wrf_british_columbia_copy_s3.py
@@ -27,7 +27,7 @@ default_args = {
     "catchup_by_default": True,
     "email_on_failure": False,
     "email_on_retry": False,
-    "retries": 0,
+    "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
 
