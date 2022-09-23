84e8aa60ff573af23f2f10f95d345ac6ea0eabef
Jeff Gregory <jeffrey.s.gregory@me.com>
2022-09-23 17:42:05 -0500

    WIP: refactor locations dag

--- dags/water/a2w_sync_locations.py
+++ dags/water/a2w_sync_locations.py
@@ -1,251 +1,187 @@
 """
-This dag needs to be refactored to only call the office API inside a task or sensor.
-
-This method of calling an API outside the task will fail when running on any system that 
-does not have proper API access (ex: Github Actions)
+# Sync CWMS RADAR Locations
 """
 
+import json
+from termios import TIOCPKT_STOP
+import pandas as pd
+from datetime import datetime, timedelta
 
-# """
-# ## Sync Water database with RADAR Locations
+import helpers.radar as radar
+import helpers.water as water
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.python import get_current_context
 
-# RADAR Locations retrieved as JSON string
-
-# ### Parse:
-
-# Extract schema needed for posting to Water API
-
-# ### Post:
-
-# Water API endpoint `/sync/locations`
-
-# ```{
-#     'office_id',
-#     'name',
-#     'public_name',
-#     'kind_id',
-#     'geometry' : {
-#         'type',
-#         'coordinates': [
-#             101, 47
-#         ]
-#     }
-# }```
-
-# __North America centriod default for missing coordinates__
-# """
-# import pprint
-# import json
-# from airflow.exceptions import AirflowFailException, AirflowSkipException
-# import numpy as np
-# import requests
-# import pandas as pd
-# from datetime import datetime, timedelta
-# from requests.api import put
-
-# from sqlalchemy.sql.operators import is_
-
-# import helpers.sharedApi as sharedApi
-# import helpers.radar as radar
-# import helpers.water as water
-
-# from airflow import AirflowException
-# from airflow.decorators import dag, task
-# from airflow.operators.python import get_current_context
+# Default arguments
+default_args = {
+    "owner": "airflow",
+    "start_date": (datetime.utcnow() - timedelta(hours=6)).replace(minute=15, second=0),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
 
 
-# pp = pprint.PrettyPrinter(indent=4)
+def json_drop_duplicates(payload):
+    df = pd.json_normalize(payload)
+    df = df.drop_duplicates(subset=["office_id", "name"], ignore_index=True)
+    df_to_json = json.loads(df.to_json(orient="records"))
+    _d = {}
+    _l = []
+    for d in df_to_json:
+        to_pop = []
+        for k, v in d.items():
+            if "." in k:
+                _k, _v = k.split(".")
+                if _k in _d:
+                    _d[_k][_v] = d[k]
+                else:
+                    _d[_k] = {_v: d[k]}
+                to_pop.append(k)
+        [d.pop(p) for p in to_pop]
+        _l.append({**d, **_d})
 
-# implementation = {
-#     "stable": {
-#         "dag_id": "a2w_sync_locations",
-#         "tags": ["a2w", "radar"],
-#     },
-#     # 'develop': {
-#     #     'bucket': 'cwbi-data-develop',
-#     #     'dag_id': 'develop_a2w_sync_locations',
-#     #     'tags': ['develop', 'a2w'],
-#     # }
-# }
-
-# # Default arguments
-# default_args = {
-#     "owner": "airflow",
-#     "start_date": (datetime.utcnow() - timedelta(hours=6)).replace(minute=15, second=0),
-#     "retries": 1,
-#     "retry_delay": timedelta(minutes=5),
-# }
-
-# # Create a DAG
-# def create_dag(**kwargs):
-#     # Assign kwargs
-#     dag_id = kwargs["dag_id"]
-#     tags = kwargs["tags"]
-#     schedule_interval = kwargs["schedule_interval"]
-#     offices = kwargs["offices"]
-
-#     # Assign result to variable 'dag'
-#     @dag(
-#         default_args=default_args,
-#         dag_id=dag_id,
-#         tags=tags,
-#         schedule_interval=schedule_interval,
-#         doc_md=__doc__,
-#     )
-#     def sync_locations():
-#         """
-#         DAG for Sync Locations
-#         """
-
-#         # Task to check if RADAR is running
-#         @task
-#         def check_radar_service():
-#             radar.check_service()
-
-#         _check_radar_service = check_radar_service()
-
-#         # Looping through each office to create a task for each
-#         for office_symbol in offices.keys():
-#             # Task to download and parse RADAR Locations
-#             @task(task_id=f"fetch_radar_{office_symbol}")
-#             def fetch_radar(office, offices, format: str = "json"):
-#                 # Acquire the locations from RADAR
-#                 # cwms_data = "https://cwms-data.usace.army.mil/cwms-data"
-#                 cwms_data = radar.RADAR_API_ROOT
-#                 url = f"{cwms_data}/locations?office={office}&name=@&format={format}"
-#                 location_kinds = json.loads(water.get_location_kind())
-#                 with requests.Session() as s:
-#                     r = s.get(url, headers={"Content-Type": "application/json"})
-#                     radar_locations_str = r.text
-#                 # Check to see if it is html
-#                 if "DOCTYPE html" in radar_locations_str:
-#                     raise AirflowSkipException
-
-#                 _radar_locations_str = radar_locations_str.translate(
-#                     radar_locations_str.maketrans("", "", "\n\r\t")
-#                 ).replace("None", "")
-#                 loc_json = json.loads(_radar_locations_str)
-#                 locations = loc_json["locations"]["locations"]
-
-#                 radar_locations = list()
-#                 for location in locations:
-#                     # Get the kind_id from the location-kind list
-#                     kind_id = [
-#                         kind["id"]
-#                         for kind in location_kinds
-#                         if kind["name"] == location["classification"]["location-kind"]
-#                     ]
-#                     office_id = offices[office]["id"]
-#                     if (
-#                         bounding_office := location["political"]["bounding-office"]
-#                     ) is not None:
-#                         if bounding_office in offices:
-#                             office_id = offices[bounding_office]["id"]
-
-#                     # Create a Location dataclass
-#                     loc = radar.Location(
-#                         office_id=office_id,
-#                         name=location["identity"]["name"],
-#                         public_name=location["label"]["public-name"],
-#                         kind_id=kind_id[0],
-#                         kind=location["classification"]["location-kind"],
-#                     )
-#                     # Create a Geometry dataclass with middle of North America as defaults
-#                     geo = radar.Geometry()
-#                     lat = location["geolocation"]["latitude"]
-#                     lon = location["geolocation"]["longitude"]
-
-#                     if isinstance(lat, float):
-#                         geo.latitude = lat
-#                     if isinstance(lon, float):
-#                         geo.longitude = lon
-
-#                     # Appending dictionary to post
-#                     radar_locations.append(
-#                         {
-#                             "office_id": loc.office_id,
-#                             "name": loc.name,
-#                             "public_name": loc.public_name,
-#                             "kind_id": loc.kind_id,
-#                             "kind": loc.kind,
-#                             "geometry": {
-#                                 "type": geo.type,
-#                                 "coordinates": [
-#                                     geo.longitude,
-#                                     geo.latitude,
-#                                 ],
-#                             },
-#                         }
-#                     )
-
-#                 radar_locations_df = pd.json_normalize(radar_locations)
-#                 radar_locations_df = radar_locations_df.drop_duplicates(
-#                     subset=["office_id", "name"], ignore_index=True
-#                 )
-
-#                 radar_locations_json = radar_locations_df.to_json(
-#                     orient="records"
-#                 ).replace("null", "None")
-
-#                 return radar_locations_json
-
-#             @task(
-#                 task_id=f"sync_to_water_{office_symbol}",
-#                 trigger_rule="none_failed_min_one_success",
-#             )
-#             def sync_to_water(locations):
-#                 is_empty = lambda v: "no value" if v is None else v
-#                 for item in eval(locations):
-#                     try:
-#                         water.sync_radar_locations(
-#                             payload={
-#                                 "office_id": item["office_id"],
-#                                 "name": item["name"],
-#                                 "public_name": is_empty(item["public_name"]),
-#                                 "kind_id": item["kind_id"],
-#                                 "geometry": {
-#                                     "type": item["geometry.type"],
-#                                     "coordinates": item["geometry.coordinates"],
-#                                 },
-#                             }
-#                         )
-#                     except Exception as err:
-#                         print(err, "\n", item)
-#                         continue
-
-#             # Tasks as objects
-#             _fetch_radar = fetch_radar(office_symbol, offices)
-
-#             _sync_to_water = sync_to_water(_fetch_radar)
-
-#             _check_radar_service >> _fetch_radar >> _sync_to_water
-
-#     # Return the created DAG to the global scope
-#     return sync_locations()
+    return _l
 
 
-# # Getting a list of offices, create a dictionary from the list, and add NWDM and NWDP
-# try:
-#     offices_json = json.loads(water.get_offices())
-# except:
-#     # This is mostly to keep local testing happy when water-api is not up.
-#     offices_json = json.loads(sharedApi.get_offices())
-# offices_dict = {d["symbol"]: d for d in offices_json if d["symbol"] is not None}
-# # Add NWDM and NWDP to the dictionary
-# for nwd in ["NWDP", "NWDM"]:
-#     if nwd == "NWDM":
-#         offices_dict[nwd] = offices_dict["NWD"]
-#     else:
-#         offices_dict[nwd] = offices_dict["NWP"]
-#     offices_dict[nwd]["symbol"] = nwd
+with DAG(
+    dag_id="a2w_sync_locations",
+    default_args=default_args,
+    schedule_interval="15 */3 * * *",
+    tags=["a2w", "radar"],
+    max_active_runs=2,
+    max_active_tasks=3,
+) as dag:
+    """
+    # Sync Water database with RADAR Locations
 
-# # Expose to the global() allowing airflow to add to the DagBag
-# for key, val in implementation.items():
-#     d_id = val["dag_id"]
-#     d_tags = val["tags"]
-#     globals()[d_id] = create_dag(
-#         dag_id=d_id,
-#         tags=d_tags,
-#         schedule_interval="15 */3 * * *",
-#         offices=offices_dict,
-#     )
+    RADAR Locations retrieved as JSON string
+
+    ## Parse:
+
+    Extract schema needed for posting to Water API
+
+    ## Post:
+
+    Water API endpoint `/sync/locations`
+
+    ```{
+        'office_id',
+        'name',
+        'public_name',
+        'kind_id',
+        'geometry' : {
+            'type',
+            'coordinates': [
+                101, 47
+            ]
+        }
+    }```
+
+    __North America centriod default for missing coordinates__
+    """
+
+    @task
+    def cwms_offices():
+        objects = water.get_offices()
+        _objects = json.loads(objects)
+        offices = [obj["symbol"] for obj in _objects if obj["symbol"] is not None]
+
+        return None if offices is [] else offices
+
+    @task
+    def add_cwms_office(offices):
+        offices[offices.index("NWD")] = "NWDM"
+        offices[offices.index("NWP")] = "NWDP"
+        return offices
+
+    @task
+    def get_locations(office):
+        locations = radar.api_request("locations", f"office={office}&names=@")
+        if locations is None or "DOCTYPE html" in locations:
+            return list()
+        locations_str = locations.translate(
+            locations.maketrans("", "", "\n\r\t")
+        ).replace("None", "")
+        locations_json = json.loads(locations_str)
+        locations_list = locations_json["locations"]["locations"]
+
+        return locations_list
+
+    @task
+    def transform_location(locations):
+        context = get_current_context()
+        ti = context["ti"]
+        locations_xcom_pull = ti.xcom_pull(task_ids=locations.task_id)
+
+        # from water, get the location kinds, offices, and make a dict
+        water_kind_ids = json.loads(water.get_location_kind())
+        kind_ids = {item["name"]: item["id"] for item in water_kind_ids}
+        water_office_ids = json.loads(water.get_offices())
+        office_ids = {item["symbol"]: item["id"] for item in water_office_ids}
+
+        radar_locations = []
+        for locations in locations_xcom_pull:
+            for location in locations:
+                kind = location["classification"]["location-kind"]
+                kind_id = kind_ids[kind.upper()]
+                office = location["identity"]["office"]
+                if office == "NWDM":
+                    office_id = office_ids["NWD"]
+                elif office == "NWDP":
+                    office_id = office_ids["NWP"]
+                else:
+                    office_id = office_ids[office]
+
+                lon = (
+                    location["geolocation"]["longitude"]
+                    if location["geolocation"]["latitude"] is not None
+                    else 101
+                )
+                lat = (
+                    location["geolocation"]["latitude"]
+                    if location["geolocation"]["latitude"] is not None
+                    else 47
+                )
+                public_name = (
+                    location["label"]["public-name"]
+                    if location["label"]["public-name"] is not None
+                    else location["identity"]["name"]
+                )
+
+                # Appending dictionary to post
+                radar_locations.append(
+                    {
+                        "office_id": office_id,
+                        "name": location["identity"]["name"],
+                        "public_name": public_name,
+                        "kind_id": kind_id,
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [
+                                lon,
+                                lat,
+                            ],
+                        },
+                    }
+                )
+
+        return radar_locations
+
+    @task
+    def post_locations(locations):
+        loc = json_drop_duplicates(locations)
+        return_message = water.sync_radar_locations(loc)
+        # for loc in locations:
+        #     return_message = water.sync_radar_locations(
+        #         payload=loc
+        #     )
+        # print(f"{return_message=}")
+
+    offices = cwms_offices()
+    added_offices = add_cwms_office(offices)
+    locations = get_locations.expand(office=added_offices)
+    transform_loc = transform_location(locations)
+    post_to_water = post_locations(transform_loc)
+
+    offices >> added_offices >> locations >> transform_loc >> post_to_water
--- plugins/helpers/radar.py
+++ plugins/helpers/radar.py
@@ -1,45 +1,15 @@
-from dataclasses import dataclass, field
-import json
-import re
+import os
+from urllib.parse import urlsplit, urlunsplit
+from airflow.models import Variable
+
 import requests
-from airflow.hooks.base import BaseHook
-from airflow.providers.http.hooks.http import HttpHook
-from airflow import AirflowException
 
-RADAR_API_ROOT = 'http://cwms-data.usace.army.mil/cwms-data'
+url_parts = urlsplit(Variable.get("CWMSDATA"))
 
-@dataclass
-class Office:
-    id: str
-    name: str
-    symbol: str
-    parent_id: str
-
-@dataclass
-class Location:
-    office_id: str = None
-    name: str = None
-    public_name: str = None
-    kind_id: str = None
-    kind: str = None
-@dataclass
-class Geometry:
-    type: str = "Point"
-    latitude: float = 0
-    longitude: float = 0
-@dataclass
-class Political:
-    nation: str = None
-    state: str = None
-    county: str = None
-    timezone: str = None
-    nearest_city: str = None
-    bounding_office: str = None
-    
-def check_service():
-    r = requests.get(f'{RADAR_API_ROOT}/offices?format=json')
+def api_request(subdirectory, query=None, fragment=None):
+    url = urlunsplit(
+        (url_parts.scheme, url_parts.netloc, f"{url_parts.path}/{subdirectory}", query, fragment)
+    )
+    r = requests.get(url)
     if r.status_code == 200:
-        return True
-    else:
-        raise AirflowException("RADAR API SERVICE DOWN!")
-##############################################################
+        return r.text
