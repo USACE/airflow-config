678e69df7f2ac4224679a5579fb6f21e78d9127d
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-09-27 21:29:55 -0400

    update

--- plugins/helpers/radar.py
+++ plugins/helpers/radar.py
@@ -14,17 +14,11 @@ def api_request(subdirectory, query=None, fragment=None):
             query,
             fragment,
         )
-        
-
-def api_request(subdirectory, query=None, fragment=None):
-    url = urlunsplit(
-        (url_parts.scheme, url_parts.netloc, f"{url_parts.path}/{subdirectory}", query, fragment)
-
     )
     r = requests.get(url)
     if r.status_code == 200:
         return r.text
-        
+
 
 def get_timeseries(tsids: list, begin: str, end: str, office=None):
     if len(tsids) == 0 or begin is None or end is None:
