62c194888ed28a4b228e1eedc4ee38f9224d0ffd
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-11-29 17:17:43 -0500

    update timeseries measurements, needs further testing

--- dags/water/a2w_sync_timeseries_measurements.py
+++ dags/water/a2w_sync_timeseries_measurements.py
@@ -43,12 +43,14 @@ def a2w_sync_timeseries_measurements():
 
         with TaskGroup(group_id=f"{office}") as task_group:
 
-            @task(task_id=f"extract_{office}_a2w_timeseries")
-            def extract_a2w_office_timeseries(office: str):
+            @task(task_id=f"a2w_{office}_enabled_timeseries")
+            def a2w_enabled_office_timeseries(office: str):
 
-                office_timeseries = water.get_cwms_timeseries(
-                    provider=office, datasource_type="cwms-timeseries", mapped=1
+                water_hook = water.WaterHook(method="GET")
+                office_timeseries = water_hook.request(
+                    endpoint=f"/timeseries?datatype=cwms-timeseries&provider={office.lower()}&etl_values_enabled=true"
                 )
+
                 # if len(office_timeseries) == 0:
                 #     raise AirflowSkipException(f"No records found for {office}")
 
@@ -69,6 +71,14 @@ def a2w_sync_timeseries_measurements():
             @task(task_id=f"extract_{office}_radar_timeseries")
             def extract_radar_timeseries(tsid):
 
+                # THIS IS NOT IDEAL, WILL CAUSE A CALL TO WATER API FOR EACH TSID
+                # get the water data source uri
+                water_hook = water.WaterHook(method="GET")
+                datasource = water_hook.request(
+                    endpoint=f"/datasources?datatype=cwms-timeseries&provider={office.lower()}"
+                )
+                uri = datasource[0]["datatype_uri"]
+
                 logging.info(f"Getting values for tsid -->  {tsid}")
 
                 # Define the extract time-windows based on the task datetime
@@ -81,11 +91,21 @@ def a2w_sync_timeseries_measurements():
                 if get_nwd_group(office) is not None:
                     radar_office = get_nwd_group(office)
 
-                r = radar.get_timeseries([tsid], begin, end, radar_office)
+                radar_hook = radar.RadarHook()
+                r = radar_hook.request_(
+                    method="GET",
+                    url=uri
+                    + f"?name={tsid}&office={radar_office.lower()}&begin={begin}&end={end}",
+                )
+                # print("-------------")
+                # print(r)
+                # print("-------------")
+
+                # r = radar.get_timeseries([tsid], begin, end, radar_office)
                 if r == None:
                     raise ValueError(f"Invalid Response: {r}")
 
-                r = json.loads(r)
+                # r = json.loads(r)
 
                 # Grab the time-series list object which can be iterated over
                 # when multiple tsids are requested
@@ -106,23 +126,21 @@ def a2w_sync_timeseries_measurements():
                     loc_payload = {}
                     # loc_payload["provider"] = ts_obj["office"].lower()
                     loc_payload["provider"] = office
-                    loc_payload["datasource_type"] = "cwms-timeseries"
+                    loc_payload["datatype"] = "cwms-timeseries"
                     loc_payload["key"] = ts_obj["name"]
                     try:
                         x = ts_obj["regular-interval-values"]["segments"][0]
                         # Note: "values" [[value, quality_code]]
-                        loc_payload["measurements"] = {
-                            "times": [x["last-time"]],
-                            "values": [x["values"][x["value-count"] - 1][0]],
-                        }
+                        loc_payload["values"] = [
+                            [x["last-time"], x["values"][x["value-count"] - 1][0]],
+                        ]
 
                     except:
                         x = ts_obj["irregular-interval-values"]["values"]
                         # Note: "values" [["time", value, quality_code]]
-                        loc_payload["measurements"] = {
-                            "times": [x[len(x) - 1][0]],
-                            "values": [x[len(x) - 1][1]],
-                        }
+                        loc_payload["values"] = [
+                            [x[len(x) - 1][0], x[len(x) - 1][1]],
+                        ]
 
                     tsid_results.append(loc_payload)
 
@@ -132,7 +150,7 @@ def a2w_sync_timeseries_measurements():
 
             @task(
                 task_id=f"load_{office}_timeseries_measurements_into_a2w",
-                trigger_rule="all_done",
+                trigger_rule="none_failed_min_one_success",
             )
             def load_timeseries_measurements_into_a2w(
                 location_timeseries_list: _LazyXComAccess,
@@ -151,7 +169,11 @@ def a2w_sync_timeseries_measurements():
 
                 if len(payload) > 0:
                     print(f"Posting {len(payload)} timeseries measurment objects")
-                    water.post_cwms_timeseries_measurements(payload)
+                    water_hook = water.WaterHook(method="POST")
+                    response = water_hook.request(
+                        endpoint=f"/providers/{office.lower()}/timeseries/values",
+                        json=payload,
+                    )
                 else:
                     raise AirflowSkipException(f"No timeseries measurements to post")
 
@@ -160,7 +182,7 @@ def a2w_sync_timeseries_measurements():
             # extract_radar_timeseries = Dynamic Task Mapping - each tsid run through extract function
             load_timeseries_measurements_into_a2w(
                 extract_radar_timeseries.expand(
-                    tsid=extract_a2w_office_timeseries(office),
+                    tsid=a2w_enabled_office_timeseries(office)
                 )
             )
 
