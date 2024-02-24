f3eb2d52911c8992aa7e981816c0c18bf36e0780
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2024-02-23 22:25:42 -0500

    extend end date

--- dags/cumulus/download_ncep_4km_stage4.py
+++ dags/cumulus/download_ncep_4km_stage4.py
@@ -28,7 +28,7 @@ default_args = {
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
-    "end_date": datetime(2019, 12, 2),
+    "end_date": datetime(2021, 12, 2),
 }
 
 
