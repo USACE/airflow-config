1b2a862d7c87466661d47e5868052b12c3ac696c
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2025-05-12 12:46:32 -0400

    WIP

--- plugins/celery_config.py
+++ plugins/celery_config.py
@@ -1,13 +1,15 @@
 import os
-from airflow.config_templates.default_celery import DEFAULT_CELERY_CONFIG
+
+# from airflow.config_templates.default_celery import DEFAULT_CELERY_CONFIG
+from airflow.configuration import conf as DEFAULT_CELERY_CONFIG
 
 # If Using SQS Message Broker, Pre-Define Queues
-if os.getenv('AIRFLOW__CELERY__BROKER_URL', "").startswith('sqs://'):
+if os.getenv("AIRFLOW__CELERY__BROKER_URL", "").startswith("sqs://"):
     BROKER_TRANSPORT_OPTIONS = {
         **DEFAULT_CELERY_CONFIG["broker_transport_options"],
         "predefined_queues": {
-                "airflow": { "url": os.getenv("CELERY_QUEUE_URL_AIRFLOW", "")}
-            }
+            "airflow": {"url": os.getenv("CELERY_QUEUE_URL_AIRFLOW", "")}
+        },
     }
 else:
     BROKER_TRANSPORT_OPTIONS = DEFAULT_CELERY_CONFIG["broker_transport_options"]
@@ -16,5 +18,5 @@ else:
 # Defined so celery_config.CELERY_CONFIG can be referenced in environment variables
 CELERY_CONFIG = {
     **DEFAULT_CELERY_CONFIG,
-    "broker_transport_options": BROKER_TRANSPORT_OPTIONS 
+    "broker_transport_options": BROKER_TRANSPORT_OPTIONS,
 }
