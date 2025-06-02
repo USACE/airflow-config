20e3497854e61201a1caae696bbd951002e914f1
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2025-06-02 11:18:30 -0400

    update office config

--- dags/wmes/wmes_hourly_jobs.py
+++ dags/wmes/wmes_hourly_jobs.py
@@ -30,13 +30,48 @@ default_args = {
 )
 def wmes_hourly_jobs():
     job_configs = [
-        {"office": "lrc", "office_group": "lrd", "enabled": False},
-        {"office": "lre", "office_group": "lrd", "enabled": False},
-        {"office": "lrh", "office_group": "lrd", "enabled": True},
-        {"office": "lrl", "office_group": "lrd", "enabled": True},
-        {"office": "lrn", "office_group": "lrd", "enabled": False},
-        {"office": "lrp", "office_group": "lrd", "enabled": False},
-        {"office": "swt", "office_group": "swd", "enabled": False},
+        {
+            "office": "lrc",
+            "office_group": "lrd",
+            "github_branch": "cwbi-dev",
+            "enabled": False,
+        },
+        {
+            "office": "lre",
+            "office_group": "lrd",
+            "github_branch": "cwbi-dev",
+            "enabled": False,
+        },
+        {
+            "office": "lrh",
+            "office_group": "lrd",
+            "github_branch": "cwbi-dev",
+            "enabled": True,
+        },
+        {
+            "office": "lrl",
+            "office_group": "lrd",
+            "github_branch": "cwbi-dev",
+            "enabled": False,
+        },
+        {
+            "office": "lrn",
+            "office_group": "lrd",
+            "github_branch": "cwbi-dev",
+            "enabled": False,
+        },
+        {
+            "office": "lrp",
+            "office_group": "lrd",
+            "github_branch": "cwbi-dev",
+            "enabled": False,
+        },
+        {
+            "office": "swt",
+            "office_group": "swd",
+            "github_branch": "cwbi-dev",
+            "enabled": False,
+        },
     ]
 
     # Organize configs by office_group
@@ -66,7 +101,10 @@ def wmes_hourly_jobs():
                         container_overrides={
                             "environment": [
                                 {"name": "OFFICE", "value": job_config["office"]},
-                                {"name": "GITHUB_BRANCH", "value": "cwbi-dev"},
+                                {
+                                    "name": "GITHUB_BRANCH",
+                                    "value": job_config["github_branch"],
+                                },
                             ],
                             "command": ["/jobs/bin/hourly.sh"],
                         },
