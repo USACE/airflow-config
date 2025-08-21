845a151617a9b94627aad8a10e5ae40c60b93f4a
Eric Novotny <nov00002@umn.edu>
2025-08-21 07:37:53 -0700

    update lpms url

--- dags/wmes/lpms_process_shef_lrd.py
+++ dags/wmes/lpms_process_shef_lrd.py
@@ -46,7 +46,7 @@ def lpms_process_shef_lrd():
 
         # LPMS SHEF LRD endpoint: https://ndc-navapps.ops.usace.army.mil/ords/lpms2/shef/LRD/yyyymmddhhmm/lookbackhours
         # lpms_url = "http://nav-app1-prod1.cwbi.lan:8080/ords/lpms2/shef/LRD"
-        lpms_url = "https://ndc-navapps.ops.usace.army.mil/ords/lpms2/shef/LRD"
+        lpms_url = "https://lpms-test.cwbi.us/ords/lpms2/shef/LRD/"
         request_url = f"{lpms_url}/{now_et}/{lookback_hours}"
 
         response = requests.get(request_url)
