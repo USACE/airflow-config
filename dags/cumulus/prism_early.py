1d52ba810b8c4641f185c0194f39a7fb2917ded7
msweier <mitchell.s.weier@usace.army.mil>
2025-12-12 14:49:08 -0600

    fix data date

--- dags/cumulus/prism_early.py
+++ dags/cumulus/prism_early.py
@@ -15,7 +15,7 @@ import helpers.cumulus as cumulus
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": (datetime.now(timezone.utc)-timedelta(days=14)).replace(
+    "start_date": (datetime.now(timezone.utc)-timedelta(days=2)).replace(
         minute=0, second=0
     ),    
     # "start_date": datetime(2021, 11, 9),
@@ -61,8 +61,8 @@ def cumulus_prism_early():
     @task()
     def download_raw_prism_early(short_name='ppt'):
         product_slug = f"prism-{short_name}-early"
-        logical_date = get_current_context()["logical_date"]-timedelta(hours=24)
-        execution_date = logical_date.date()
+        logical_date = get_current_context()["logical_date"]
+        execution_date = (get_current_context()["data_interval_end"]).date()-timedelta(hours=24)
         results = []
 
         dt = execution_date
